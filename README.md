15ee6bb779079fd5163591d00a714ac2a95c77ed
Klaudia <56263298+klaudiajaros@users.noreply.github.com>
2021-02-16 21:20:09 +0000

    modified pom.xml, Dockerfiles, travis and App.java to connect to mySQL database and run SQL queries to view an employee record

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## SEM - Software Engineering Methods repository
 SET08103 2020-1 TR2 001 - Software Engineering Methods 2021
 
-working with IntelliJ, Docker and Travis CI
+working with IntelliJ, mySQL, Docker and Travis CI
+
+The purpose of the application is to manage employee records for a company. The system will primarily used by the Human Resources Department (HR) and managers. An existing SQL database exists with this data.
 
 - Master Build Status [![Build Status](https://travis-ci.com/KlaudiaJaros/sem_repo.svg?branch=master)](https://travis-ci.com/KlaudiaJaros/sem_repo)
 - Develop Build Status [![Build Status](https://travis-ci.com/KlaudiaJaros/sem_repo.svg?branch=master)](https://travis-ci.com/KlaudiaJaros/sem_repo)
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,29 +1,151 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+import java.sql.*;
 
-public class App {
+public class App
+{
+    /*
+    Connection to MySQL database
+     */
+    private static Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /*
+    Disconnect from the mySQL database
+     */
+    public void disconnect(){
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT E.emp_no, E.first_name, E.last_name, T.title, T.to_date, S.salary, S.to_date, "
+                            + "D.dept_name, DM.emp_no, DM.to_date, E2.first_name, E2.last_name "
+                            + "FROM employees E "
+                            + "JOIN titles T ON (E.emp_no=T.emp_no)"
+                            + "JOIN salaries S ON (E.emp_no=S.emp_no)"
+                            + "JOIN dept_emp DE ON (E.emp_no=DE.emp_no)"
+                            + "JOIN departments D ON (DE.dept_no=D.dept_no)"
+                            + "JOIN dept_manager DM ON (D.dept_no=DM.dept_no)"
+                            + "JOIN employees E2 On (DM.emp_no=E2.emp_no)"
+                            + "WHERE E.emp_no = " + ID
+                            + " ORDER BY T.to_date DESC, S.to_date DESC, DM.to_date DESC"
+                            + " LIMIT 1";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("E.emp_no");
+                emp.first_name = rset.getString("E.first_name");
+                emp.last_name = rset.getString("E.last_name");
+                emp.title=rset.getString("T.title");
+                emp.salary=rset.getInt("S.salary");
+                emp.dept_name=rset.getString("D.dept_name");
+                emp.manager=rset.getString("E2.first_name") + " " + rset.getString("E2.last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+        else{
+            System.out.println("employee null");
+        }
+    }
+
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2021")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+        // Create a new app:
+        App a = new App();
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+        //connect to database:
+        a.connect();
+
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+
+        // disconnect:
+        a.disconnect();
     }
 }
